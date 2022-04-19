2c9a1eaba53b054a46d82e4d49174beb8c40aefb
kittiphop <kittiphopzheng@hotmail.com>
2022-04-19 16:36:28 +0700

    added "button selected" and change color on buytton hover feature

--- DBApps/FormMain.Designer.cs
+++ DBApps/FormMain.Designer.cs
@@ -30,22 +30,37 @@
         {
             this.panelNavigation = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.labelSetting = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.panelNavigation.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // panelNavigation
             // 
             this.panelNavigation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.panelNavigation.Controls.Add(this.pictureBox6);
+            this.panelNavigation.Controls.Add(this.pictureBox5);
+            this.panelNavigation.Controls.Add(this.pictureBox4);
+            this.panelNavigation.Controls.Add(this.pictureBox3);
+            this.panelNavigation.Controls.Add(this.pictureBox2);
             this.panelNavigation.Controls.Add(this.pictureBox1);
-            this.panelNavigation.Controls.Add(this.label6);
+            this.panelNavigation.Controls.Add(this.labelSetting);
             this.panelNavigation.Controls.Add(this.label5);
             this.panelNavigation.Controls.Add(this.label4);
             this.panelNavigation.Controls.Add(this.label2);
@@ -61,91 +76,95 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::DBApps.Properties.Resources.ic_fluent_settings_24_filled__2_;
-            this.pictureBox1.Location = new System.Drawing.Point(29, 514);
+            this.pictureBox1.Location = new System.Drawing.Point(25, 514);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(19, 19);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // label6
+            // labelSetting
             // 
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label6.Location = new System.Drawing.Point(12, 506);
-            this.label6.Name = "label6";
-            this.label6.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
-            this.label6.Size = new System.Drawing.Size(191, 33);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Settings";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label6.Click += new System.EventHandler(this.label_Click);
-            this.label6.MouseEnter += new System.EventHandler(this.label_MouseEnter);
-            this.label6.MouseLeave += new System.EventHandler(this.label_MouseLeave);
+            this.labelSetting.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelSetting.Location = new System.Drawing.Point(12, 507);
+            this.labelSetting.Name = "labelSetting";
+            this.labelSetting.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.labelSetting.Size = new System.Drawing.Size(191, 33);
+            this.labelSetting.TabIndex = 0;
+            this.labelSetting.Text = "Settings";
+            this.labelSetting.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelSetting.Click += new System.EventHandler(this.label_Click);
+            this.labelSetting.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.labelSetting.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label5
             // 
-            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 251);
+            this.label5.Location = new System.Drawing.Point(12, 248);
             this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
             this.label5.Size = new System.Drawing.Size(191, 33);
             this.label5.TabIndex = 0;
             this.label5.Text = "System";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.label5.Click += new System.EventHandler(this.label_Click);
             this.label5.MouseEnter += new System.EventHandler(this.label_MouseEnter);
             this.label5.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label4
             // 
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(12, 203);
             this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
             this.label4.Size = new System.Drawing.Size(191, 33);
             this.label4.TabIndex = 0;
             this.label4.Text = "System";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Click += new System.EventHandler(this.label_Click);
             this.label4.MouseEnter += new System.EventHandler(this.label_MouseEnter);
             this.label4.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label2
             // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Location = new System.Drawing.Point(12, 113);
             this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
             this.label2.Size = new System.Drawing.Size(191, 33);
             this.label2.TabIndex = 0;
             this.label2.Text = "System";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label_Click);
             this.label2.MouseEnter += new System.EventHandler(this.label_MouseEnter);
             this.label2.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label3
             // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 154);
+            this.label3.Location = new System.Drawing.Point(12, 158);
             this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
             this.label3.Size = new System.Drawing.Size(191, 33);
             this.label3.TabIndex = 0;
             this.label3.Text = "System";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Click += new System.EventHandler(this.label_Click);
             this.label3.MouseEnter += new System.EventHandler(this.label_MouseEnter);
             this.label3.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label1
             // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 68);
             this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
             this.label1.Size = new System.Drawing.Size(191, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "System";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label_Click);
             this.label1.MouseEnter += new System.EventHandler(this.label_MouseEnter);
             this.label1.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
@@ -161,6 +180,61 @@
             this.panel2.TabIndex = 1;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DBApps.Properties.Resources.ic_fluent_settings_24_filled__2_;
+            this.pictureBox2.Location = new System.Drawing.Point(25, 255);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::DBApps.Properties.Resources.ic_fluent_settings_24_filled__2_;
+            this.pictureBox3.Location = new System.Drawing.Point(25, 210);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::DBApps.Properties.Resources.ic_fluent_settings_24_filled__2_;
+            this.pictureBox4.Location = new System.Drawing.Point(25, 165);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(19);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::DBApps.Properties.Resources.ic_fluent_settings_24_filled__2_;
+            this.pictureBox5.Location = new System.Drawing.Point(25, 120);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(19);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 1;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::DBApps.Properties.Resources.ic_fluent_settings_24_filled__2_;
+            this.pictureBox6.Location = new System.Drawing.Point(25, 75);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(19);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
@@ -172,10 +246,16 @@
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
             this.Name = "FormMain";
             this.Text = "Form1";
             this.panelNavigation.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -185,12 +265,17 @@
         private System.Windows.Forms.Panel panelNavigation;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelSetting;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- DBApps/FormMain.cs
+++ DBApps/FormMain.cs
@@ -15,11 +15,18 @@ namespace DBApps
         public FormMain()
         {
             InitializeComponent();
+            //create a new panel use for display accent color when navigation button(label) is selected
+            //delete these 3 lines if use the "Dispose solution code"
+            leftBorderAccentColor = new Panel();
+            leftBorderAccentColor.Size = new Size(4, 15);
+            panelNavigation.Controls.Add(leftBorderAccentColor);
         }
 
         private Label lblCurrent;
+        private Panel leftBorderAccentColor;
         private Color clickedColor = Color.FromArgb(234, 234, 234);
         private Color nonClickColor = Color.FromArgb(243, 243, 243);
+        private Color accentColor = Color.FromArgb(0,120,212);
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
             Color color = Color.FromArgb(243, 243, 243);//240,243,249
@@ -30,6 +37,7 @@ namespace DBApps
         private void label_MouseEnter(object sender, EventArgs e)
         {
             Label label = (Label)sender;
+
             if (label.BackColor == clickedColor)
             {
                 return;
@@ -55,23 +63,37 @@ namespace DBApps
         //change all the label's BackColor to nonClickColor
         private void ReverseLabelColor()
         {
-            foreach(Control lblAsButtons in panelNavigation.Controls)
+            if(lblCurrent != null)
             {
-                if(lblAsButtons is Label)
-                {
-                    lblAsButtons.BackColor = nonClickColor;
-                }
+                lblCurrent.BackColor = nonClickColor;
+                leftBorderAccentColor.BackColor = nonClickColor;
+                /* "Dispose solution code" (remove the "leftBorderAccentColor.BackColor = nonClickColor;" line first)
+                 * Dispose the panel when new navigation button(label) is selected
+                    leftBorderAccentColor.Dispose();
+                */
             }
         }
 
-        //change all the label's BackColor to nonClickColor, then change the Clicked label to clickedColor 
+        //change all the label's BackColor to nonClickColor, then change the Clicked label to clickedColor and add leftBorderAccentColor
         private void ChangeColorOnEvent(object sender)
         {
-            if(sender != null && lblCurrent != (Label)sender)
+            if(sender != null) //&& lblCurrent != (Label)sender)
             {
                 ReverseLabelColor();
                 lblCurrent = (Label)sender;
                 lblCurrent.BackColor = clickedColor;
+
+                /* "Dispose solution code" (put these code on top of leftBorderAccentColor.BackColor = accentColor;)
+                 * create a panel by triggering the event then Dispose later when new Navigation button(label) is selected
+                    leftBorderAccentColor = new Panel();
+                    leftBorderAccentColor.Size = new Size(5, 25);
+                    panelNavigation.Controls.Add(leftBorderAccentColor);
+                */
+
+                leftBorderAccentColor.BackColor = accentColor;
+                leftBorderAccentColor.Location = new Point(13, lblCurrent.Location.Y+9);
+                leftBorderAccentColor.Visible = true;
+                leftBorderAccentColor.BringToFront();
             }
         }
     }
