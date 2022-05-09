de26d6f5d667e5fc6ad5a84671bfd19ccfbfb44c
kittiphop <kittiphopzheng@hotmail.com>
2022-05-09 13:41:55 +0700

    added custom component

---
+++ DBApps/OvalPictureBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace DBApps
+{
+    class OvalPictureBox : PictureBox
+    {
+        public OvalPictureBox()
+        {
+            this.BackColor = Color.FromArgb(230, 230, 230);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            using (var gp = new GraphicsPath())
+            {
+                gp.AddEllipse(new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+                this.Region = new Region(gp);
+            }
+        }
+    }
+}
